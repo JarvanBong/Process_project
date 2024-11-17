c8f0286678857680f543b57449f6cbf7ab7aa046
JarvaneBong <772025037@qq.com>
2024-11-17 17:39:32 +0800

    third  commit

--- demo.c
+++ demo.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main () {
-    int a = 3, b = 6;
+    int a = 6, b = 6;
     float c;
     c = (float)a /  (float)b;
     printf("c = %f\n",c);
+    printf("计算结束");
     return 0;
 }
